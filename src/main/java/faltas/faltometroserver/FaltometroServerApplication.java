56b830741eb840bbc4af02fd2ccfdf29bfa9f21b
Lucas Acosta <lucas.heck@live.com>
2025-06-03 18:41:28 -0300

    initial commit

---
+++ src/main/java/faltas/faltometroserver/FaltometroServerApplication.java
@@ -0,0 +1,13 @@
+package faltas.faltometroserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaltometroServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaltometroServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/faltas/faltometroserver/FaltometroServerApplicationTests.java
@@ -0,0 +1,13 @@
+package faltas.faltometroserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaltometroServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
